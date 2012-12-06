8903247850a1ec4e11fa2dc23b402abcae0387a2
Kambiz Darabi <darabi@m-creations.net>
2012-12-06 22:08:28 +0000

    Importing some symbols which are not exported in current asdf
    Change-Id: I1c834da0f051383639be660ac277b8bc3b16af6e

--- source/package.lisp
+++ source/package.lisp
@@ -9,6 +9,12 @@
 (defpackage :hu.dwim.asdf
   (:use :asdf
         :common-lisp)
+  (:import-from :asdf
+           #:coerce-pathname
+           #:directory-pathname-p
+           #:getenv
+           #:merge-pathnames*
+           #:subdirectories)
   (:export #:find-and-load-swank-integration-systems
            #:hu.dwim.system
            #:hu.dwim.test-system
